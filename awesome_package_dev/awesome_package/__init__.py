3c8c7b1e8db5bc1a93b5349905f9db92b27f94e3
Matthew Mulholland <mmulholland@ets.org>
2020-08-15 22:08:59 -0400

    Add class

--- awesome_package_dev/awesome_package/__init__.py
+++ awesome_package_dev/awesome_package/__init__.py
@@ -1,4 +1,4 @@
-from .awesome_rust_extension import hello_from_rust
+from .awesome_rust_extension import hello_from_rust, Word
 
 def hello_from_python():
     print("Hello world from Python!")
--- awesome_package_dev/src/lib.rs
+++ awesome_package_dev/src/lib.rs
@@ -6,8 +6,25 @@ fn hello_from_rust() {
     println!("Hello World from Rust!");
 }
 
+#[pyclass]
+struct Word {
+    #[pyo3(get, set)]
+    word: String,
+    #[pyo3(get, set)]
+    lemma: String
+}
+
+#[pymethods]
+impl Word {
+    #[new]
+    fn new(word: String, lemma: String) -> Self {
+        Word { word, lemma }
+    }
+}
+
 #[pymodule]
 fn awesome_rust_extension(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(hello_from_rust))?;
+    m.add_class::<Word>()?;
     Ok(())
 }
